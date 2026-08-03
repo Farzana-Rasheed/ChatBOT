7f523ca3baece99ac82aed081b97c72d89123c69
Farzana Rasheed <farzanarasheed559@gmail.com>
2026-08-03 00:05:30 -0700

    Implement basic chatbot functionality with Streamlit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+from google import genai
+import streamlit as st
+import os 
+st.title("AptechBTK CHAT BOT SYSTEM APKY SAWAL OR UNKA TASALI BAKHS JAwAB")
+
+googleAPIKEY=os.get_env("meriAPI_key")
+
+
+client = genai.Client(api_key=googleAPIKEY)
+
+userKaSawal= st.text_input(label="Enter Your Question Here ")
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents=userKaSawal
+)
+# print(response.text)
+
+if st.button("Submit"):
+    st.markdown(response.text)
